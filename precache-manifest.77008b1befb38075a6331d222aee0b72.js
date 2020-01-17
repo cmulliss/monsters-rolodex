3fe33285e5ebf0e6e4eb0c128508956849dc8c25
cmulliss <cmulliss@gmail.com>
2020-01-17 17:46:59 +0000

    Updates

---
+++ precache-manifest.77008b1befb38075a6331d222aee0b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60115bec9b162ac98cd8c7550a393fdb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84d8e2670ce14608ec7a",
+    "url": "/monsters-rolodex/static/css/main.0b16375e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84d8e2670ce14608ec7a",
+    "url": "/monsters-rolodex/static/js/main.128ca5ab.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
